c4449602f81eafc0f6a04d3b4e87aa231f3a013a
sgomc <macobii4@gmail.com>
2025-09-27 00:49:21 +0100

    init commit

---
+++ cv-analyzer-api/src/main/java/com/example/cv_analyzer_api/CvAnalyzerApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.cv_analyzer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvAnalyzerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvAnalyzerApiApplication.class, args);
+		
+	}
+
+}
---
+++ cv-analyzer-api/src/test/java/com/example/cv_analyzer_api/CvAnalyzerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cv_analyzer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvAnalyzerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
